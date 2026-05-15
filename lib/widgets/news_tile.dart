eeee4f45bc484cfe44882ac9ba5d97a5ca1928a9
Parawita <60746262+Parawita@users.noreply.github.com>
2026-05-15 04:36:49 +0300

    Update news_tile.dart

---
+++ lib/widgets/news_tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+
+class NewsTile extends StatelessWidget {
+  const NewsTile({super.key, required this.articleModel});
+
+  final ArticleModel articleModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              articleModel.image ??
+                  'https://via.placeholder.com/400x200.png?text=No+Image',
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Container(
+                  height: 200,
+                  width: double.infinity,
+                  color: Colors.grey.shade300,
+                  child: const Icon(
+                    Icons.image_not_supported,
+                    size: 50,
+                    color: Colors.grey,
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            articleModel.title ?? 'No Title',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            articleModel.subTitle ?? 'No Description',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
